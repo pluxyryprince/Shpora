69672190a1f6930a93c6c22ac6508d29db9c4788
pluxury golf <pluxury golf@DESKTOP-SV9M877>
2021-05-12 23:40:14 +0300

    Добавьте файлы проекта.

---
+++ Shpora/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+namespace Shpora
+{
+    public class Commi
+    {
+        public string Name { get; set; }
+    }
+    public partial class MainPage : ContentPage
+    {
+        public List<Commi> Commies { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            Commies = new List<Commi>
+            {
+                new Commi {Name ="Самое основное"},
+                   new Commi {Name ="Двойные углы"},
+                      new Commi {Name ="Синус суммы, косинус разности"},
+                         new Commi {Name ="Сумма синусов, разность косинусов"},
+                            new Commi {Name ="Тройные углы"}
+            };
+            this.BindingContext = this;
+        }
+
+        private void CommiesList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Page1 page1 = new Page1();
+            Navigation.PushAsync(page1);
+            page1.content(e.SelectedItemIndex);
+        }
+    }
+}
---
+++ Shpora/Page1.cs
@@ -0,0 +1,35 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Shpora
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        public void content(int temp)
+        {
+            switch (temp)
+            {
+                case 0:
+                    one.IsVisible = true;
+                    break;
+                case 1:
+                    two.IsVisible = true;
+                    break;
+                case 2:
+                    three.IsVisible = true;
+                    break;
+                case 3:
+                    four.IsVisible = true;
+                    break;
+                case 4:
+                    five.IsVisible = true;
+                    break;
+            }
+        }
+    }
+}
